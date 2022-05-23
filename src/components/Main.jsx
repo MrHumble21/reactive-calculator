043c34bbf973e9076cb2a77c6a532cca3f0b6d46
MrHumble21 <malikovabdulboriy010@gmail.com>
2022-05-23 16:33:44 +0500

    code refining

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,9 +3,7 @@ const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, "."];
 const firstSide = [];
 const digits = [];
 function Main() {
-  const numberHandler = () => {
-    // setDisplay()
-  };
+  
   const [Display, setDisplay] = useState("");
   return (
     <>
